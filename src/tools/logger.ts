658d91112ffe9217126fc75136e73cba9089ad37
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-04-06 09:38:10 -0300

    Se crea logger.ts

---
+++ src/tools/logger.ts
@@ -0,0 +1,28 @@
+import winston from 'winston';
+
+class Logger{
+	logger = winston.createLogger({
+		format: winston.format.simple(),
+		transports: [
+			new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+			new winston.transports.File({ filename: 'logs/warn.log', level: 'warn' }),
+			new winston.transports.Console({
+				format: winston.format.combine(winston.format.colorize(), winston.format.simple())
+			})
+		]
+	});
+
+	warn(mensaje){
+		this.logger.warn(mensaje);
+	}
+
+	error(mensaje){
+		this.logger.error(mensaje);
+	}
+
+	info(mensaje){
+		this.logger.info(mensaje);
+	}
+}
+
+export const logger = new Logger();
