bcde08840ad4551b69dc44a22ec8476e861279b0
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-04-06 09:01:54 -0300

    Remove test

--- src/tools/GEOTools.test.ts
+++
@@ -1,7 +0,0 @@
-import { GEOTools } from "./GEOTools";
-
-// Verifico la obtencion de coordenadas con una provincia
-test('Pruebas GEOTools obtener coordenadas', async () => {
-  var coordenadas =  await GEOTools.getCoordenadas("Cordoba");
-  expect(coordenadas).toStrictEqual({"lat":-32.142932663607,"lon":-63.8017532741662});
-});
