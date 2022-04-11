6ac4e2ad0f51241d9d47e2ff1624d59be4db128b
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-04-11 10:56:53 -0300

    Fix positio Actual Expected

--- test/apis/datosARAPI.test.ts
+++ test/apis/datosARAPI.test.ts
@@ -2,12 +2,12 @@ import {DARAPI} from '../../src/apis/datosARAPI';
 
 test('DARAPI - GET Córdoba', async() => {
 	const prov = await DARAPI.provincia('cordoba');
-	expect('Córdoba').toBe(prov.provincias[0].nombre);
-	expect(1).toBe(prov.cantidad);
+	expect(prov.provincias[0].nombre).toBe('Córdoba');
+	expect(prov.cantidad).toBe(1);
 });
 
 test('DARAPI - GET Tucumán - Nombre', async() => {
 	const prov = await DARAPI.provincia('tucuman');
-	expect('Tucumán').toBe(prov.provincias[0].nombre);
-	expect(1).toBe(prov.cantidad);
+	expect(prov.provincias[0].nombre).toBe('Tucumán');
+	expect(prov.cantidad).toBe(1);
 });
--- test/apis/ipAPI.test.ts
+++ test/apis/ipAPI.test.ts
@@ -2,10 +2,10 @@ import {IpAPI} from '../../src/apis/ipAPI';
 
 test('IpAPI - Buenos Aires', async() => {
 	const data = await IpAPI.get('186.111.141.96');
-	expect('Buenos Aires').toBe(data.city);
+	expect(data.city).toBe('Buenos Aires');
 });
 
 test('IpAPI - Toronto', async() => {
 	const data = await IpAPI.get('172.67.156.222');
-	expect('Toronto').toBe(data.city);
+	expect(data.city).toBe('Toronto');
 });
